d3e38bae736941e7308855e6200ed4e8573c4bc9
Denise Henderson <denise@henderson3.us>
2020-08-16 00:52:55 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -58,6 +58,11 @@ require 'pry'
     artists.each {|artist| puts "#{artists.index(artist) + 1}. #{artist}"}
   end
 
+  def list_genres
+    sorted_library = self.library.sort_by {|song|song.genre.name}
+    genres = sorted_library.collect {|song|"#{song.genre.name}"}.uniq
+    genres.each {|genre| puts "#{genres.index(genre) + 1}. #{genre}"}
+  end
 
 
 
